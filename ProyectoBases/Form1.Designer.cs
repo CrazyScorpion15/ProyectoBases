e95f2e7c38d8458e6f8a9c15ba6ff338bd4895fa
Edryan López <ealopezla@correo.url.edu.gt>
2024-10-18 12:03:42 -0600

    Creacion de las ventanas para todas las operaciones necesarias

--- ProyectoBases/Form1.Designer.cs
+++ ProyectoBases/Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            txtUsuario = new TextBox();
+            label3 = new Label();
+            txtContraseña = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(79, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(111, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Inicio de sesion";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Ingrese usuario";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(127, 41);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(151, 27);
+            txtUsuario.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 92);
+            label3.Name = "label3";
+            label3.Size = new Size(135, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Ingrese Contraseña";
+            // 
+            // txtContraseña
+            // 
+            txtContraseña.Location = new Point(153, 89);
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.Size = new Size(125, 27);
+            txtContraseña.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(96, 156);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 5;
+            button1.Text = "Continuar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(297, 197);
+            Controls.Add(button1);
+            Controls.Add(txtContraseña);
+            Controls.Add(label3);
+            Controls.Add(txtUsuario);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtUsuario;
+        private Label label3;
+        private TextBox txtContraseña;
+        private Button button1;
     }
 }
